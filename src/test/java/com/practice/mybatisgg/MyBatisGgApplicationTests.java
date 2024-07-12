7f856ee487f107ce4c28cbb83128189baed6b511
Bingnan <u6508459@anu.edu.au>
2024-07-12 11:09:56 +0800

    init

---
+++ src/test/java/com/practice/mybatisgg/MyBatisGgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.mybatisgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisGgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
