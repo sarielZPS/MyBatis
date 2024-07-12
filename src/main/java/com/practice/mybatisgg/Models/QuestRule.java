1bf09c6f959bf40b2e3ab526f5839a6f21835762
Bingnan <u6508459@anu.edu.au>
2024-07-12 14:49:51 +0800

    CRUD

---
+++ src/main/java/com/practice/mybatisgg/Models/QuestRule.java
@@ -0,0 +1,83 @@
+package com.practice.mybatisgg.Models;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class QuestRule {
+    private String id;
+    private String eventCode;
+    private String name;
+    private int status;
+    private Date createdDate;
+    private String createdBy;
+    private Date updateDate;
+    private String updateBy;
+
+    public QuestRule() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEventCode() {
+        return eventCode;
+    }
+
+    public void setEventCode(String eventCode) {
+        this.eventCode = eventCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+}
--- src/main/java/com/practice/mybatisgg/MyBatisGgApplication.java
+++ src/main/java/com/practice/mybatisgg/MyBatisGgApplication.java
@@ -1,9 +1,11 @@
 package com.practice.mybatisgg;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.practice.mybatisgg.Mapper")
 public class MyBatisGgApplication {
 
     public static void main(String[] args) {
